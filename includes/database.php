a1fbf947c9044741adc1ef3d7931eaf33c6ef992
Meg Fryling <mfryling@siena.edu>
2019-10-13 17:09:28 -0400

    Add files via upload

--- includes/database.php
+++ includes/database.php
@@ -1,1 +1,19 @@
+<!-- Meg Fryling -->
+<!-- October 2019 -->
 
+<?php
+//Connect to Database
+//This is the older method but works for PHP version installed on oraserv. 
+define('DB_SERVER','localhost');
+define('DB_PORT','3306');
+define('DB_USERNAME','your-username'); //need to update
+define('DB_PASSWORD','your-password'); //need to update
+define('DB_NAME','your-db-name'); //need to update
+ 
+$dbh = mysql_connect(DB_SERVER.':'.DB_PORT,DB_USERNAME,DB_PASSWORD);
+if (!$dbh) {
+    echo "Oops! Something went horribly wrong.";
+    exit();
+}
+
+mysql_selectdb(DB_NAME,$dbh);
